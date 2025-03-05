1162b6a1e3949326279a47333a86e2ab01c89ef4
John-77 <john.vas131@gmail.com>
2025-03-05 12:30:57 -0500

    reconfigures folders for backend

---
+++ backend/controllers/tourController.js
@@ -0,0 +1,4 @@
+exports.testController = (req, res) => {
+    console.log(req)
+    res.status(200).send('Hello fromm the server side! - controller2');  
+}    
--- controllers/tourController.js
+++
@@ -1,4 +0,0 @@
-exports.testController = (req, res) => {
-    console.log(req)
-    res.status(200).send('Hello fromm the server side! - controller');  
-}    
