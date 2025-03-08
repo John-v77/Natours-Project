1bc90e559bc92b6d7f021f35c4646683a47e7097
John-77 <john.vas131@gmail.com>
2025-03-08 15:38:34 -0500

    updates to and advanced Tour model

--- backend/models/tourModel.js
+++ backend/models/tourModel.js
@@ -6,14 +6,51 @@ const TourSchema = new Schema({
         required: [true, 'Add a name for Tours'],
         unique: true
     },
-    rating: {
+    duration: {
+        type: Number,
+        required: [true, 'a tour must have a duration'],
+    },
+    maxGroupSize: {
+        type: String,
+        required: [true, 'a tour must have a group size'],
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'a tour must have a dificulty'],
+    },
+    ratingAverate: {
         type: Number,
         default: 4.5
+    },
+    ratingQuantity: {
+        type: Number,
+        default: 0
     },
     price: {
         type: Number,
         required: [true, 'Add price for tour package'],
     },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true,
+        required: [true, 'A tour must have a description'],
+
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    imageCover: {
+        type: String,
+        required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    startDates: [Date]
 })
 
 
