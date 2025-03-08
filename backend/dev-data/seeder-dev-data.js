2e324138e3c1da22cfb39bd2b52f158e4e601548
John-77 <john.vas131@gmail.com>
2025-03-08 16:15:59 -0500

    adds script to seed DB

---
+++ backend/dev-data/seeder-dev-data.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('../models/tourModel');
+const colors = require('colors')
+
+dotenv.config({ path: './config.env' })
+
+const DB = process.env.DATABASE
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful!'));
+
+// Read Json File
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/tours-simple.json`, 'utf-8')
+);
+
+
+// Import data into DB
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+}
+
+
+// Delete all data from DB
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+}
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--del') {
+  deleteData();
+}
