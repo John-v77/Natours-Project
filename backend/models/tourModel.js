b67f3102eab23013873fb2d14d7b46930428667d
John-77 <john.vas131@gmail.com>
2025-03-10 19:03:06 -0400

    implements query/model middleware

--- backend/models/tourModel.js
+++ backend/models/tourModel.js
@@ -53,7 +53,11 @@ const tourSchema = new Schema({
         default: Date.now(),
         select: false
     },
-    startDates: [Date]
+    startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    }
 },
     {
         toJSON: { virtuals: true },
@@ -81,6 +85,19 @@ tourSchema.pre('save', function (next) {
 //     console.log(doc);
 //     next();
 // })
+
+tourSchema.pre(/^find/, function (next) {
+    this.find({ secretTour: { $ne: true } });
+    this.start = Date.now();
+    next();
+})
+
+
+tourSchema.post(/^find/, function (docs, next) {
+    console.log(`Query took ${Date.now() - this.start} milliseconds!`)
+    console.log(docs)
+    next();
+})
 
 
 const Tour = model('Tour', tourSchema)
