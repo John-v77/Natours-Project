6550fd3f16686fce39214de92211dbe448aa973a
John-77 <john.vas131@gmail.com>
2025-03-16 20:13:56 -0400

    adds errorController

---
+++ backend/controllers/errorController.js
@@ -0,0 +1,68 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+}
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+  const message = `Duplicate field value: ${value}. Please use another value!`
+  return new AppError(message, 400)
+}
+
+const handleValidationErrorDB = err => {
+  const error = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input data. ${error.join('. ')}`;
+  return new AppError(message, 400);
+}
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  })
+}
+
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    })
+
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // log error
+    console.error('Error *', err);
+
+    // send generic error
+    res.status(500).json({
+      status: 'error',
+      message: 'Something wend very wrong!'
+    })
+  }
+}
+
+module.exports = (err, req, res, next) => {
+  console.log(process.env.NODE_ENV, "vvvvv".red)
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let errorCopy = { ...err };
+    if (errorCopy.name === 'CastError') { errorCopy = handleCastErrorDB(errorCopy) };
+    sendErrorProd(errorCopy, res)
+    if (errorCopy.code === 11000) { errorCopy = handleDuplicateFieldsDB(errorCopy) };
+    sendErrorProd(errorCopy, res)
+    if (errorCopy.name === 'ValidationError') { errorCopy = handleValidationErrorDB(errorCopy) };
+    sendErrorProd(errorCopy, res)
+  }
+}
