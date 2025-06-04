8cd4d0e0f4ee3e17a4e34cff7f12651f02375a14
John-77 <john.vas131@gmail.com>
2025-06-03 22:15:01 -0400

    add mapbox to EsBundle

--- backend/public/js/index.js
+++ backend/public/js/index.js
@@ -1,9 +1,22 @@
 console.log('Hello from parcel21')
 import '@babel/polyfill';
-import { login } from "./login";
+import { login, logout } from "./login";
+import { displayMap } from './mapbox';
 
 // // DOM elements
+const mapBox = document.getElementById('map');
+
 const loginForm = document.querySelector('.form');
+const logOutBtn = document.querySelector('.nav__el--logout');
+console.log(logOutBtn);
+
+
+
+if (mapBox) {
+  const locations = JSON.parse(mapBox.dataset.locations);
+  console.log(locations, "locations");
+  displayMap(locations);
+}
 
 // DELEGATION
 if (loginForm) {
@@ -15,7 +28,11 @@ if (loginForm) {
   })
 }
 
-document.querySelector('.form').addEventListener('submit', e => {
-  e.preventDefault();
-  login(email, password);
-});
+if (logOutBtn) { logOutBtn.addEventListener('click', logout) };
+
+// document.querySelector('.form').addEventListener('submit', e => {
+//   e.preventDefault();
+//   login(email, password);
+// });
+
+
