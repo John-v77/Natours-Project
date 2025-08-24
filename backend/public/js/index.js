b8f95735122863eef3947c0bf3568e54330fa5b4
John-77 <john.vas131@gmail.com>
2025-08-24 10:23:00 -0400

    adds signup form

--- backend/public/js/index.js
+++ backend/public/js/index.js
@@ -1,4 +1,5 @@
 import '@babel/polyfill';
+import { signup } from './signup';
 import { login, logout } from "./login";
 import { displayMap } from './mapbox';
 import { updateSettings } from './updateSettings';
@@ -6,7 +7,12 @@ import { bookTour } from './stripe';
 // // DOM elements
 const mapBox = document.getElementById('map');
 
+
+
+// DOM ELEMENTS
+
 const loginForm = document.querySelector('.form');
+const signupForm = document.querySelector('.form--signup');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
 const userPasswordForm = document.querySelector('.form-user-password');
@@ -31,6 +37,19 @@ if (loginForm) {
 }
 
 if (logOutBtn) { logOutBtn.addEventListener('click', logout) };
+
+// DELEGATION
+if (signupForm) {
+  console.log(signupForm, "signupForm");
+  signupForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('passwordConfirm').value;
+    signup(name, email, password, passwordConfirm);
+  });
+}
 
 if (userDataForm) {
   userDataForm.addEventListener('submit', e => {
