ce6f8cf7c5ddc04b9c538810770cbedf88664aea
John-77 <john.vas131@gmail.com>
2025-09-07 18:14:38 -0400

    adds news pages for my account

--- backend/routes/viewRoutes.js
+++ backend/routes/viewRoutes.js
@@ -9,6 +9,10 @@ router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/signup', authController.isLoggedIn, viewsController.getSignupForm);
 router.get('/me', authController.protect, viewsController.getAccount);
+router.get('/settings', authController.protect, viewsController.getSettings);
+router.get('/my-bookings', authController.protect, viewsController.getMyBookings);
+router.get('/my-reviews', authController.protect, viewsController.getMyReviews);
+router.get('/billing', authController.protect, viewsController.getBilling);
 
 router.post('/submit-user-data', authController.protect, viewsController.updateUserData);
 
